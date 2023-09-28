ab0880c4cf0c42cfeecf0a9f0eeef1f103ac0095
zebrrrra <s4bas589@hotmail.com>
2023-09-28 21:37:47 +0800

    fix: modify nearbysearch url

--- app/api/frontend/nearbySearch.js
+++ app/api/frontend/nearbySearch.js
@@ -1,5 +1,5 @@
 export const nearbySearch = async ({ keyword, lat, lng, encodeOptions }) => {
-  const url = `http://localhost:3000/api/search?keyword=${keyword}&lat=${lat}&lng=${lng}&options=${encodeOptions}`;
+  const url = `https://food-map-sandy.vercel.app/api/search?keyword=${keyword}&lat=${lat}&lng=${lng}&options=${encodeOptions}`;
   const response = await fetch(url);
   const data = await response.json();
 
