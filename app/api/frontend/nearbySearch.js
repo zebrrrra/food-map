057d2eb35bbe8273ce1e852a4223b9b76175770e
zebrrrra <s4bas589@hotmail.com>
2023-09-28 22:25:03 +0800

    fix: search url (github page)

--- app/api/frontend/nearbySearch.js
+++ app/api/frontend/nearbySearch.js
@@ -1,5 +1,5 @@
 export const nearbySearch = async ({ keyword, lat, lng, encodeOptions }) => {
-  const url = `https://food-map-sandy.vercel.app/api/search?keyword=${keyword}&lat=${lat}&lng=${lng}&options=${encodeOptions}`;
+  const url = `https://zebrrrra.github.io/food-map/api/search?keyword=${keyword}&lat=${lat}&lng=${lng}&options=${encodeOptions}`;
   const response = await fetch(url);
   const data = await response.json();
 
