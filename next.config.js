ac7c98dbc79b7fae9a11eeb4b371afaa890b1812
zebrrrra <s4bas589@hotmail.com>
2023-09-28 21:07:08 +0800

    fix: next.config

--- next.config.js
+++ next.config.js
@@ -1,21 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
     domains: ["lh3.googleusercontent.com", "img.icons8.com"],
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "maps.googleapis.com",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "img.icons8.com",
-        port: "",
-        pathname: "/**",
-      },
-    ],
   },
 };
 
